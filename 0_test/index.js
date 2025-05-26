b069e07c24c4797d932712b9cd6dd66d12f44fae
sudeep prajapati <sudeeppprajapati@gmail.com>
2025-05-26 18:27:44 +0530

    Add initial JavaScript files and notes on file types and execution

---
+++ 0_test/index.js
@@ -0,0 +1,15 @@
+"use strict"; // treat all js code as newer version
+
+console.log("Hello");
+
+
+// alert(3 + 3) // we are using node.js, note browser
+
+console.log(3
+    - 3); console.log("Sudeep");// Low readability bad practice
+
+/*
+console.log(3 - 3); 
+console.log("Sudeep");
+High readability best practice
+ */
