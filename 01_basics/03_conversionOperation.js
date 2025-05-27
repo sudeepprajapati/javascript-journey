4e811b0aa15b0e337e48fb41d3ce8e4f66fb8630
sudeep prajapati <sudeeppprajapati@gmail.com>
2025-05-27 18:57:51 +0530

    Refactor

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -34,5 +34,75 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 33;
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+/* ********************** Operations ******************************************** */
+
+let value = 3
+let nagValue = -value
+// console.log(nagValue);
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 2); //power
+// console.log(2 / 2);
+// console.log(2 % 2); // reminder // usecase in crypto currency and other algos
+
+let str1 = "hello"
+let str2 = 'sudeep'
+
+let str3 = str1 + str2
+// console.log(str3);
+
+
+// console.log("1" + 2);
+// console.log((1 + "2"));
+// console.log("1" + 2 + 3); // "12" + 3 = 123
+// console.log(1 + 2 + "3"); // 3 + "3" = 33
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+// gameCounter++;
+// ++gameCounter;
+console.log(gameCounter);
+
+// prefix and postfix js 
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+// console.log(`a:${a}, b:${b}`);
+
+// Expected output: "a:4, b:4"
+
+// Postfix increment
+// let x = 3;
+// const y = x++;
+// // x is 4; y is 3
+
+// let x2 = 3n;
+// const y2 = x2++;
+// // x2 is 4n; y2 is 3n
+
+// Prefix increment
+// let x = 3;
+// const y = ++x;
+// // x is 4; y is 4
+
+// let x2 = 3n;
+// const y2 = ++x2;
+// // x2 is 4n; y2 is 4n
