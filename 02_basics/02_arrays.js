552ec1e50868a77bf5add79d31da4da4107267f1
sudeep prajapati <sudeeppprajapati@gmail.com>
2025-06-05 14:59:51 +0530

    Add array

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,34 @@
+const marvelHeros = ["thor", "ironman", "spiderman"]
+const dcHeros = ["superman", "flash", "batman"]
+
+// marvelHeros.push(dcHeros) // push add whole array as a datatype to main array 
+
+// console.log(marvelHeros);
+// console.log(marvelHeros[3][2]);
+
+// const collab = marvelHeros.concat(dcHeros) //contact add new array's values to new array without affectin original one
+// console.log(collab);
+
+// spread oprater
+
+// const collab2 = [...marvelHeros, ...dcHeros]
+// console.log(collab2);
+
+
+// const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+
+// const real_another_array = another_array.flat(Infinity) // it needs depth as parameter 
+
+// console.log(real_another_array);
+
+
+// console.log(Array.isArray("Sudeep"));
+// console.log(Array.from("Sudeep")); // conversion
+// console.log(Array.from({ name1: "sudeep" })); // interview needs to clarify
+
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3));
