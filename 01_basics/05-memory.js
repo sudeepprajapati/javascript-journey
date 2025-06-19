7ab4e574bc55b068dc8decdc32a98dfaf6c34cb2
sudeep prajapati <sudeeppprajapati@gmail.com>
2025-06-19 18:49:25 +0530

    Refine comments

--- 01_basics/05-memory.js
+++ 01_basics/05-memory.js
@@ -27,10 +27,10 @@ console.log(userTwo.email);
 
 // 🧠 Stack (Primitive Types):
 // - Used for storing primitive data types: string, number, boolean, null, undefined, symbol, bigint
-// - Stores the actual value
-// - Fast access and auto-managed
 // - When you assign one primitive to another, a **copy** of the value is made-----
 // - Changes to one variable do NOT affect the other
+// - Stores the actual value
+// - Fast access and auto-managed
 
 // 🧱 Heap (Non-Primitive Types):
 // - Used for storing non-primitive data types: objects, arrays, functions
